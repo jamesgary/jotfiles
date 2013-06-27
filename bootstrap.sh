d308cc37abf4588a079f60f7c631df709495ee10
James Gary <mrjamesgary@gmail.com>
2013-06-27 18:22:57 +0000

    Fix thingies

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,13 +11,13 @@ $aptget install -y vim git-core
 
 # homeshick
 rm -rf $HOME/.homesick
-git clone git://github.com/andsens/homeshick.git  $repo_dir/homeshick
-git clone git://github.com/jamesgary/jotfiles.git $repo_dir/jotfiles
+git clone git@github.com:andsens/homeshick.git  $repo_dir/homeshick
+git clone git@github.com:jamesgary/jotfiles.git $repo_dir/jotfiles
 $repo_dir/homeshick/home/.homeshick link
 
 # vundle
 rm -rf $HOME/.vim/bundle/vundle
-git clone git://github.com/gmarik/vundle.git $HOME/.vim/bundle/vundle
+git clone git@github.com:gmarik/vundle.git $HOME/.vim/bundle/vundle
 vim +BundleInstall +qall
 
 # source it!
